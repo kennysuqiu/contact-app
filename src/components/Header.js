ace7304b80c6e4f3adc71a140637cde5dd6e1adb
Kenny Su Qiu <kfernandosuqiu@gmail.com>
2022-06-29 16:20:49 -0400

    All content before React Router

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui menu">
+      <div className="ui container centered grid" style={{ marginTop: "5px" }}>
+        <h2>Contact Manager</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
